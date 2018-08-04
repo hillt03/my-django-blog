be5e84adbe4b9ca8ff197ddf5668b92d606592b8
Tim Hill <hillt03@yahoo.com>
2018-08-04 00:31:23 -0400

    static fix

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
--- mysite/settings.py
+++ mysite/settings.py
@@ -118,5 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = ' /home/hillt03/hillt03.pythonanywhere.com/static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "./blog/static/"),
+]
