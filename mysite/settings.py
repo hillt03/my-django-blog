a7594cb6fc1ba175e49a002ef77f66f6959ec636
Tim Hill <hillt03@yahoo.com>
2018-08-05 16:36:26 -0400

    added base html file and extended post_list

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = ' /home/hillt03/hillt03.pythonanywhere.com/static'
+STATIC_URL = ' /home/hillt03/hillt03.pythonanywhere.com/static/' # production
+#STATIC_URL = '/static/' # development
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
